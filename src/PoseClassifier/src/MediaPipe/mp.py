3d87b08ec3fcd51b94ad59c6a5c2b09de0b8db92
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2021-11-30 17:24:51 +0100

    Refactoring #1

--- src/PoseClassifier/src/MediaPipe/mp.py
+++ src/PoseClassifier/src/MediaPipe/mp.py
@@ -54,6 +54,7 @@ class PoseMP:
         self.data = []
         self.results = None
 
+
     def classify_image(self, image: np.ndarray, image_id: str = "000", reshape: bool = False):
         self.image_id = image_id
         self.image = image
@@ -62,25 +63,25 @@ class PoseMP:
         self.get_pose()
         self.prepare_data(w, h)
         self.dump_data()
-        if self.visualize:
-            self.visualize_pose()
+        # if self.visualize:
+        #     self.visualize_pose()
 
-
+    #@lru_cache
     def prepare_data(self, width, height):
 
         # list world postions and fill in missing values
-
+        
         positions = self.lisify_and_fill_missing_landmarks()
 
-        # positions = [self.results.pose_world_landmarks.landmark[self.mp_pose.PoseLandmark[name.upper()]]
-        #              for name in self.landmark_names]
+        if positions:
+            landmarks = [[position.x, position.y, position.z]
+                        for position in positions]
+            names = [name.upper()for name in self.landmark_names]
+            self.data = [{names[i]: landmark}
+                        for i, landmark in enumerate(landmarks)]
 
-        landmarks = [[position.x, position.y, position.z]
-                     for position in positions]
-        names = [name.upper()for name in self.landmark_names]
-        self.data = [{names[i]: landmark}
-                     for i, landmark in enumerate(landmarks)]
-
+        #print(len(self.data))
+        #print(self.data)
         #self.data = [{name: self.results.pose_world_landmarks.landmark[mp_pose.PoseLandmark[name.upper()]]} for name in self.landmark_names]
         self.data.append({"image_id": self.image_id})
         self.data.append({"image_width": width})
@@ -103,7 +104,7 @@ class PoseMP:
                 w/(h/DESIRED_HEIGHT)), DESIRED_HEIGHT))
         return img, h, w
 
-    @lru_cache
+    
     def get_pose(self):
         with self.mp_pose.Pose(
                 static_image_mode=self.static_image_mode,
@@ -117,12 +118,12 @@ class PoseMP:
             self.results = pose.process(
                 cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB))
 
-    @lru_cache
+    
     def dump_data(self) -> None:
-        with open("./landmarks.txt", "w+") as outfile:
+        with open("../landmarks.txt", "w+") as outfile:
             json.dump(self.data, outfile, indent=4)
 
-    @lru_cache
+    
     def lisify_and_fill_missing_landmarks(self) -> list:
         """
         Wenn man die Thresholds min_detection_confidence und min_tracking_confidence
@@ -130,37 +131,42 @@ class PoseMP:
         Für diesen fall könnte man die fehlenden Einträge durch die vergangenen ersetzen.
         """
         positions = []
-        if self.results.pose_landmarks is None:
-            return self.previous_positions
+        if not self.results.pose_landmarks:
+            if self.previous_positions:
+                return self.previous_positions
+            else:
+                return None 
         
+        position = None
         for ind, name in enumerate(self.landmark_names):
             
+            #try:
             position = self.results.pose_landmarks.landmark[self.mp_pose.PoseLandmark[name.upper(
-            )]]
+                )]]
+            
+            # except AttributeError:
+            #     #single missing value
+            #     if len(self.previous_positions)>ind and self.previous_positions:
+            #         position = self.previous_positions[ind]
 
             # fill in missing values with old value
             if position is None:
                 self.missing_counter += 1
                 print(f"Missing Counter {self.missing_counter}")
-                posititon = self.previous_positions[ind]
+                position = self.previous_positions[ind]
             positions.append(position)
-
-        self.previous_postitions = positions
+        assert len(positions)==len(self.landmark_names)
+        self.previous_positions = positions
         return positions
 
-    def visualize_pose(self):
-
-        self.mp_drawing.draw_landmarks(
-            self.image,
-            self.results.pose_world_landmarks,
-            self.mp_pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=self.mp_drawing_styles.get_default_pose_landmarks_style())
-
-
-
+ 
 def main(use_img):
-    pose = PoseMP(visualize=True, static_image_mode=False, smooth_landmarks=False,
-                 smooth_segmentation=False, min_detection_confidence=0.5,
+
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_pose = mp.solutions.pose
+    pose = PoseMP(visualize=False, static_image_mode=False, smooth_landmarks=True,
+                 smooth_segmentation=True, min_detection_confidence=0.5,
                  min_tracking_confidence=0.5, enable_segmentation=False, model_complexity=2,
                  landmark_names=['left_shoulder', 'right_shoulder',
                                  'left_elbow', 'right_elbow',
@@ -173,40 +179,42 @@ def main(use_img):
     
     count = 0
     start = 0
-    total_time = 0.0
-    if not use_img:
-        try:
-            cap = cv2.VideoCapture(0)
-            
-            while cap.isOpened(): 
 
-                if cv2.waitKey(5) & 0xFF == 27:
-                    break
-                
-                success, image = cap.read()
-                
-                if not success:
-                    # ignore empty frames
-                    continue
-                start = time.perf_counter()
-                count += 1
-                image.flags.writeable = False
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                pose.classify_image(image)
-                end = time.perf_counter()
-                total_time += end-start
-                
-            cap.release()
-            if total_time:
-                print(f"performance: {count/total_time}")
+    if not use_img:
+
+        cap = cv2.VideoCapture(0)
+        start = time.perf_counter()
+        while cap.isOpened(): 
+
+            success, image = cap.read()
+        
+            if not success:
+                # ignore empty frames
+                continue
             
-        except KeyboardInterrupt:
-            end = time.perf_counter()
-            total_time = end-start
+            count += 1
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            pose.classify_image(image,image_id=count)
             
-            print(f"performance: {count/(total_time*1000)}")
-            if total_time:
-                return count/total_time
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(
+            image,
+            pose.results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+            # Flip the image horizontally for a selfie-view display.
+            
+            cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+            if cv2.waitKey(5) & 0xFF == 27:
+                return
+            
+        cap.release()
+        end = time.perf_counter()
+        
+        print(f"performance: {count/end-start}")
+
 
 
 if __name__ == "__main__":
