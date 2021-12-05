ee0ab3f0a87a3c041fd8408eea3731344118a015
Tobias Schafberger <t.schafberger@oth-aw.de>
2021-12-05 16:49:10 +0100

    Implemented controller for data (de)serialization.

---
+++ src/Desktop/DesktopApplication/src/PTSC.Communication/Controller/DataController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using PTSC.Communication.Model;
+using PTSC.Interfaces;
+using Unity;
+
+namespace PTSC.Communication.Controller
+{
+    public class DataController
+    {
+        [Dependency] public ILogger Logger { get; set; }
+
+        public string SerializeDriverData(object obj)
+        {
+            string serialOutput = string.Empty;
+            IList<PropertyInfo> props = new List<PropertyInfo>(obj.GetType().GetProperties()); // list of object properties
+
+            foreach (var prop in props)
+            {
+                // check if property is of enumerable type
+                if (prop.PropertyType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(prop.PropertyType))
+                {
+                    IList values = prop.GetValue(obj, null) as IList;
+                    serialOutput += prop.Name + ";";
+                    foreach (var val in values)
+                    {
+                        serialOutput += val.ToString() + ";";
+                    }
+                }
+                else
+                {
+                    serialOutput += prop.Name + ";";
+                    serialOutput += prop.GetValue(obj, null).ToString() + ";";
+                }
+            }
+            Logger.Log("Serialized data: " + serialOutput);
+            return serialOutput;
+        }
+
+        public object DeserializeModuleData(string jsonString)
+        {
+            return JsonSerializer.Deserialize<ModuleDataModel>(jsonString);
+        }
+    }
+}
