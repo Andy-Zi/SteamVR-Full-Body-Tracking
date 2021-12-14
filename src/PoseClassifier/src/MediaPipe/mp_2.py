78e85df74a37669fd43fd0e19ba249bacda26d6c
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2021-12-14 10:37:54 +0100

    plot points

--- src/PoseClassifier/src/MediaPipe/mp_2.py
+++ src/PoseClassifier/src/MediaPipe/mp_2.py
@@ -1,6 +1,6 @@
 import mediapipe as mp
 import cv2
-
+import time
 from utils_mp.positions import Positions
 import numpy as np
 
@@ -25,12 +25,13 @@ class PoseMP:
         self.mp_pose = mp.solutions.pose.Pose(estimator_options)
 
     def classify_image(self, image: np.ndarray, image_id: str = "000"):
+        
         pose = self._get_pose(image)
 
-        if pose is not None and pose.pose_landmarks is not None:
-
+        if pose is not None and pose.pose_world_landmarks is not None:
+            
             self.points.manage_points(pose)
-            self.points.previous_positions = self.points.points
+            #self.points.previous_positions = self.points.points
             return pose
         else:
             return None
@@ -40,6 +41,7 @@ class PoseMP:
 
 
 def main():
+    runs = 100
     pose = PoseMP()
 
     mp_drawing = mp.solutions.drawing_utils
@@ -52,7 +54,7 @@ def main():
     count = 0
 
     while cap.isOpened():
-
+        runs -= 1
         success, image = cap.read()
 
         if not success:
@@ -61,7 +63,10 @@ def main():
 
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        start = time.time()
         results = pose.classify_image(image, image_id=count)
+        end = time.time()
+        print(f"FPS: {1/(end-start)}")
         if results is None:
             continue
         if results.pose_landmarks is not None:
@@ -77,6 +82,8 @@ def main():
             cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
             if cv2.waitKey(5) & 0xFF == 27:
                 return
+        if runs < 10:
+            return
 
 
 if __name__ == "__main__":
