60f6e9d37fa60f8a5e4b6b2a3fc2226e58a8d819
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2021-12-16 16:46:52 +0100

    refactoring with use of dataclass

--- src/Modules/PoseClassifier/MediaPipe/utils_mp/positions.py
+++ src/Modules/PoseClassifier/MediaPipe/utils_mp/positions.py
@@ -1,64 +1,49 @@
 from typing import Optional
 import json
 import mediapipe as mp
-import numpy as np
+from ...utils.positions_dataclass import Positions
+from dataclasses import fields
 
 
-class Positions:
+class PositionHandler:
+    """ use keys from Positions-Class to extract positions from classification
+
+    Returns:
+        [Postisions]: [Dataclass with elemets as landmarks ans values of type list[float]]
+    """
     defaultPosition: list[float] = [.0, .0, .0]
     position_visible_threshold: float = 0.5
+    positions:dict[str,list[float]] = {field.name:[] for field in fields(Positions())}
+    previous_positions: Positions = Positions()
+    current_positions: Positions = Positions()
+    
 
-    def __init__(self, ignore_hidden_points:Optional[bool]=False, outputfile: str = "/Users/macbook/Documents/KI_Master/AR-VR/arvr-projekt-modularbeit/src/Modules/PoseClassifier/landmarks.txt", inputfile: str = "/Users/macbook/Documents/KI_Master/AR-VR/arvr-projekt-modularbeit/src/Modules/PoseClassifier/keypoints.txt"):
+    def __init__(self, ignore_hidden_points: Optional[bool] = False, outputfile: str = "/Users/macbook/Documents/KI_Master/AR-VR/arvr-projekt-modularbeit/src/Modules/PoseClassifier/landmarks.txt", inputfile: str = "/Users/macbook/Documents/KI_Master/AR-VR/arvr-projekt-modularbeit/src/Modules/PoseClassifier/keypoints.txt"):
         # settings
-        self.use_visibility_threshold = ignore_hidden_points   
+        self.use_visibility_threshold = ignore_hidden_points
         self.input_file = inputfile
         self.export_file = outputfile
-        self.keys = self._read_point_names()
 
-        # helping vars
+        # keep track of not visible points (error message in VR: "body part is not visible from camera")
         self.not_visible_names: list[str] = []
-        self.points: dict[str, list[float]] = {key: [] for key in self.keys}
-        self.num_landmarks = len(self.keys)
 
-        self.previous_positions: list[list[float]] = []
         self.mp_pose = mp.solutions.pose
-        self.current_positions: list[str] = []
 
-    def manage_points(self, results)-> dict[str, list[float]]:
-        """loads keypoint names from keypoints.txt and fills them with values"""
+    def manage_points(self, results) -> Positions:
+        """loads keypoint names from dataclass Positions and fills them with values"""
+        self.last_positions = self.current_positions
+        landmarks = self._load_positions(results=results)
 
-        positions = self._load_positions(results=results)
-
-        if positions == []:
-            return
-        landmarks = self._calc_landmarks(positions=positions)
-        
-        self._prepare_data(landmarks=landmarks)
-        
-        return self.points
-        # self.write_file()
+        self._calc_landmarks(landmarks)
+        if self.positions:
+            self.current_positions = Positions(**self.positions)
+            return self.current_positions
 
     def _load_positions(self, results):
         """ extracts the landmarks from the result"""
-        positions: list[list[float]] = []
-        position: list[float] = []
-        #PoseLandmarks = [str(x).split(".")[1].lower() for x in self.mp_pose.PoseLandmark]
-        #print(f"{PoseLandmarks}")
-        for ind, name in enumerate(self.keys):
-            try:
-                position = results.pose_world_landmarks.landmark[self.mp_pose.PoseLandmark[name.upper(
-                )]]
-            except AttributeError:
-                # fill in single missing value with last position
-                if len(self.previous_positions) > ind and self.previous_positions:
-                    position = self.previous_positions[ind]
-                    self._set_name_warning(name)
-
-            finally:
-                positions.append(position)
-
-        assert len(positions) == len(self.keys)
-        
+        positions: dict[str,list[float]] = {}
+        for field in fields(Positions):
+            positions[field.name] =  results.pose_world_landmarks.landmark[self.mp_pose.PoseLandmark[field.name]]
         return positions
 
     def _set_name_warning(self, name: str):
@@ -66,7 +51,7 @@ class Positions:
         self.not_visible_names.append(name)
         print(f"{name=} caused an AttributeError \n")
 
-    def _calc_landmarks(self, positions: list[float]):
+    def _calc_landmarks(self, landmarks):
         """listify positions
 
         Returns:
@@ -75,36 +60,27 @@ class Positions:
 
         # werte von der Sichtbarkeit abhängig:
         # position.visibility<self.position_visibility_threshold setzt einen default wert
-        if not positions:
+        
+        if not landmarks:
             return
-        landmarks: list[list[float]] = []
-        for i, position in enumerate(positions):
-            if position:
-                if self.use_visibility_threshold and position.visibility < self.position_visible_threshold:
-                    landmarks.append(self.defaultPosition)
+        
+        for key, value in landmarks.items():
+            print(f"{key=}\t{value=}")
+            if not self.positions:
+                print("Key not in positions, continuing ...")
+                break
+            if value:
+                if self.use_visibility_threshold and value.visibility < self.position_visible_threshold:
+                    self.positions[key] = self.defaultPosition
                 else:
-                    # , self.dist(position,index=i)])
-                    landmarks.append([position.x, position.y, position.z])
+                    self.positions[key] = [value.x, value.y, value.z, value.visibility]
             else:
                 continue
-        self.previous_position = landmarks
-        return landmarks
-
-
-
-    def _prepare_data(self, landmarks: list[list[float]]) -> dict[str,Optional[list[float]]]:
-        """fills in points for keys
-
-        Args:
-            landmarks (list[float]): [list of points: x,y,z]
-        """
-        for ind, key in enumerate(self.keys):
-            if landmarks:
-                self.points[key] = landmarks[ind]
+            
 
     def write_file(self):
         with open(self.export_file, "w+") as f:
-            json.dump(self.points, f, indent=4)
+            json.dump(self.current_positions, f, indent=4)
 
     def _read_point_names(self):
         """
