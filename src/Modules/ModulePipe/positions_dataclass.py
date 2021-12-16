50d0e100e61c5bb1ee68bebebbd4de2f9cf067e2
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2021-12-16 10:55:03 +0100

    changed default value

--- src/Modules/ModulePipe/positions_dataclass.py
+++ src/Modules/ModulePipe/positions_dataclass.py
@@ -2,41 +2,41 @@ from dataclasses import dataclass, asdict
 
 @dataclass
 class Positions:
-    NOSE            : list[float] = []
-    LEFT_EYE_INNER  : list[float] = []
-    LEFT_EYE        : list[float] = []
-    LEFT_EYE_OUTER  : list[float] = []
-    RIGHT_EYE_INNER : list[float] = []
-    RIGHT_EYE       : list[float] = []
-    RIGHT_EYE_OUTER : list[float] = []
-    LEFT_EAR        : list[float] = []
-    RIGHT_EAR       : list[float] = []
-    MOUTH_LEFT      : list[float] = []    
-    MOUTH_RIGHT     : list[float] = []
-    LEFT_SOULDER    : list[float] = []
-    RIGHT_SHOULDER  : list[float] = []
-    LEFT_ELBOW      : list[float] = []
-    RIGHT_ELBOW     : list[float] = []
-    LEFT_WRIST      : list[float] = []
-    RIGHT_WRIST     : list[float] = []
-    LEFT_PINKY      : list[float] = []
-    RIGHT_PINKY     : list[float] = []
-    LEFT_INDEX      : list[float] = []
-    RIGHT_INDEX     : list[float] = []
-    LEFT_THUMB      : list[float] = []
-    RIGHT_THUMB     : list[float] = []
-    LEFT_HIP        : list[float] = []
-    RIGHT_HIP       : list[float] = []
-    LEFT_KNEE       : list[float] = []
-    RIGHT_KNEE      : list[float] = []
-    LEFT_ANKLE      : list[float] = []
-    RIGHT_ANKLE     : list[float] = []
-    LEFT_HEEL       : list[float] = []
-    RIGHT_HEEL      : list[float] = []
-    LEFT_FOOT       : list[float] = []
-    RIHT_FOOT       : list[float] = []
-    LEFT_FOOT_INDEX : list[float] = []
-    RIGHT_FOOT_INDEX: list[float] = []
+    NOSE            : list[float] = None
+    LEFT_EYE_INNER  : list[float] = None
+    LEFT_EYE        : list[float] = None
+    LEFT_EYE_OUTER  : list[float] = None
+    RIGHT_EYE_INNER : list[float] = None
+    RIGHT_EYE       : list[float] = None
+    RIGHT_EYE_OUTER : list[float] = None
+    LEFT_EAR        : list[float] = None
+    RIGHT_EAR       : list[float] = None
+    MOUTH_LEFT      : list[float] = None    
+    MOUTH_RIGHT     : list[float] = None
+    LEFT_SOULDER    : list[float] = None
+    RIGHT_SHOULDER  : list[float] = None
+    LEFT_ELBOW      : list[float] = None
+    RIGHT_ELBOW     : list[float] = None
+    LEFT_WRIST      : list[float] = None
+    RIGHT_WRIST     : list[float] = None
+    LEFT_PINKY      : list[float] = None
+    RIGHT_PINKY     : list[float] = None
+    LEFT_INDEX      : list[float] = None
+    RIGHT_INDEX     : list[float] = None
+    LEFT_THUMB      : list[float] = None
+    RIGHT_THUMB     : list[float] = None
+    LEFT_HIP        : list[float] = None
+    RIGHT_HIP       : list[float] = None
+    LEFT_KNEE       : list[float] = None
+    RIGHT_KNEE      : list[float] = None
+    LEFT_ANKLE      : list[float] = None
+    RIGHT_ANKLE     : list[float] = None
+    LEFT_HEEL       : list[float] = None
+    RIGHT_HEEL      : list[float] = None
+    LEFT_FOOT       : list[float] = None
+    RIHT_FOOT       : list[float] = None
+    LEFT_FOOT_INDEX : list[float] = None
+    RIGHT_FOOT_INDEX: list[float] = None
 
     def serialize(self):
         return asdict(self)
