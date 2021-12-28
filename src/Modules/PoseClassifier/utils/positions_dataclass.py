2911d60681453d77bcc718d23bb8a3dd276191d1
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2021-12-28 16:17:27 +0100

    update

--- src/Modules/PoseClassifier/utils/positions_dataclass.py
+++ src/Modules/PoseClassifier/utils/positions_dataclass.py
@@ -3,38 +3,22 @@ from typing import List
 @dataclass
 class Positions:
     NOSE            : List[float] = None
-    #LEFT_EYE_INNER  : List[float] = None
     LEFT_EYE        : List[float] = None
-    #LEFT_EYE_OUTER  : List[float] = None
-    #RIGHT_EYE_INNER : List[float] = None
     RIGHT_EYE       : List[float] = None
-    #RIGHT_EYE_OUTER : List[float] = None
     LEFT_EAR        : List[float] = None
     RIGHT_EAR       : List[float] = None
-    #MOUTH_LEFT      : List[float] = None    
-    #MOUTH_RIGHT     : List[float] = None
     LEFT_SHOULDER   : List[float] = None
     RIGHT_SHOULDER  : List[float] = None
     LEFT_ELBOW      : List[float] = None
     RIGHT_ELBOW     : List[float] = None
     LEFT_WRIST      : List[float] = None
     RIGHT_WRIST     : List[float] = None
-    # LEFT_PINKY      : List[float] = None
-    # RIGHT_PINKY     : List[float] = None
-    # LEFT_INDEX      : List[float] = None
-    # RIGHT_INDEX     : List[float] = None
-    # LEFT_THUMB      : List[float] = None
-    # RIGHT_THUMB     : List[float] = None
     LEFT_HIP        : List[float] = None
     RIGHT_HIP       : List[float] = None
     LEFT_KNEE       : List[float] = None
     RIGHT_KNEE      : List[float] = None
     LEFT_ANKLE      : List[float] = None
     RIGHT_ANKLE     : List[float] = None
-    #LEFT_HEEL       : List[float] = None
-    #RIGHT_HEEL      : List[float] = None
-    #LEFT_FOOT_INDEX : List[float] = None
-    #RIGHT_FOOT_INDEX: List[float] = None
 
     def serialize(self):
         return asdict(self)
