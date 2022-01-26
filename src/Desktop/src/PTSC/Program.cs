c28ff6bee769aebc0226507b0ad0165bbc24bff5
Andreas Ziegler <andyziegler@gmx.de>
2022-01-26 16:39:18 +0100

    Merge branch 'main' of https://git.oth-aw.de/arvr-ws2021/arvr-projekt-modularbeit

--- src/Desktop/src/PTSC/Program.cs
+++ src/Desktop/src/PTSC/Program.cs
@@ -23,68 +23,97 @@ namespace PTSC
         {
             var applicationEnvironment =  new ApplicationEnvironment();
 
-            var executablePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            string moduleDirectory = executablePath;
+            var executableDirecory = System.AppContext.BaseDirectory;
 
+            var logger = new Logger(executableDirecory);
+            logger.Log($"Base Path: {executableDirecory}");
+            string moduleDirectory = executableDirecory;
 
+            try
+            {
 #if DEBUG
-            //Get 'src'-Directory of Repo
-            for (int i = 0; i < 4; i++)
-                moduleDirectory = Directory.GetParent(moduleDirectory).FullName;
+                logger.Log("Running in Debug Mode");
+                //Get 'src'-Directory of Repo
+                for (int i = 0; i < 6; i++)
+                    moduleDirectory = Directory.GetParent(moduleDirectory).FullName;
 
-            moduleDirectory = Path.Combine(moduleDirectory, "Modules");
+                moduleDirectory = Path.Combine(moduleDirectory, "Modules");
 
 #else
-            // In release Mode we expect a "Modules" Directory next to the *.exe
-            moduleDirectory = Path.Combine(moduleDirectory, "Modules");
+                logger.Log("Running in Release Mode");
+                // In release Mode we expect a "Modules" Directory next to the *.exe
+                moduleDirectory = Path.Combine(moduleDirectory, "Modules");
 #endif
+            }
+            catch (Exception e)
+            {
+                logger.Log(e.Message);
+            }
+
 
 
             applicationEnvironment.ModulesDirectory = moduleDirectory;
-            applicationEnvironment.SettingsPath = Path.Combine(executablePath, "ApplicationSettings.json");
-
-            //Try to Load the Settings
-            if (File.Exists(applicationEnvironment.SettingsPath))
+            applicationEnvironment.SettingsPath = Path.Combine(executableDirecory, "ApplicationSettings.json");
+            
+            try
             {
-                var settings = JsonSerializer.Deserialize<ApplicationSettingsModel>(File.ReadAllText(applicationEnvironment.SettingsPath));
-                settings.ResetState();
-                applicationEnvironment.Settings = settings;
+                //Try to Load the Settings
+                if (File.Exists(applicationEnvironment.SettingsPath))
+                {
+                    var settings = JsonSerializer.Deserialize<ApplicationSettingsModel>(File.ReadAllText(applicationEnvironment.SettingsPath));
+                    settings.ResetState();
+                    applicationEnvironment.Settings = settings;
+                }
+                else
+                {
+                    //Create New
+                    applicationEnvironment.Settings = new ApplicationSettingsModel().Default();
+                    File.WriteAllText(applicationEnvironment.SettingsPath, JsonSerializer.Serialize(applicationEnvironment.Settings, new JsonSerializerOptions() { WriteIndented = true }));
+                }
+            }catch (Exception e)
+            {
+                logger?.Log(e.Message);
             }
-            else{
-                //Create New
-                applicationEnvironment.Settings = new ApplicationSettingsModel().Default();
-                File.WriteAllText(applicationEnvironment.SettingsPath, JsonSerializer.Serialize(applicationEnvironment.Settings,new JsonSerializerOptions() { WriteIndented = true}));
+
+            logger?.Log("Building Container");
+
+            try
+            {
+                var container = new UnityContainer();
+
+
+                applicationEnvironment.LoggingPath = logger.LoggingPath;
+
+                container.RegisterInstance<ILogger>(logger);
+                container.RegisterInstance<ILogger>("ModuleDataLogger", new Logger(executableDirecory, "moduleData.txt"));
+                container.RegisterInstance<ILogger>("DriverDataLogger", new Logger(executableDirecory, "driverData.txt"));
+
+                container.RegisterInstance<IApplicationEnvironment>(applicationEnvironment);
+
+                var repo = container.Resolve<ModuleRepository>(new ResolverOverride[] { new ParameterOverride("moduleDirectory", moduleDirectory) });
+                repo.Load();
+                container.RegisterInstance(repo);
+
+                container.RegisterType<MainView, MainView>();
+                container.RegisterType<MainController, MainController>();
+
+
+                container.RegisterType<DriverPipeServerController, DriverPipeServerController>(new ContainerControlledLifetimeManager());
+                container.RegisterType<DriverPipeServerController, DriverPipeServerController>(new ContainerControlledLifetimeManager());
+                container.RegisterType<DataController, DataController>(new ContainerControlledLifetimeManager());
+                container.RegisterType<ProcessingPipeline, ProcessingPipeline>(new ContainerControlledLifetimeManager());
+                container.RegisterType<ModuleWrapper, ModuleWrapper>(new ContainerControlledLifetimeManager());
+                container.RegisterInstance<IKalmanFilterModel>(new KalmanFilterModel());
+
+                ApplicationConfiguration.Initialize();
+                var controller = container.Resolve<MainController>();
+                Application.Run(controller.RegisterEventAggregator(container).Initialize().View);
+            }catch(Exception e)
+            {
+                logger.Log("Fatal Error!");
+                logger.Log(e.Message);
             }
-
-            var container = new UnityContainer();
-
-            var logger = new Logger(executablePath);
-            applicationEnvironment.LoggingPath = logger.LoggingPath;
-
-            container.RegisterInstance<ILogger>(logger);
-            container.RegisterInstance<ILogger>("ModuleDataLogger", new Logger(executablePath, "moduleData.txt"));
-            container.RegisterInstance<ILogger>("DriverDataLogger", new Logger(executablePath, "driverData.txt"));
-
-            container.RegisterInstance<IApplicationEnvironment>(applicationEnvironment);
-
-            var repo = container.Resolve<ModuleRepository>(new ResolverOverride[] { new ParameterOverride("moduleDirectory", moduleDirectory) });
-            repo.Load();
-            container.RegisterInstance(repo);
-
-            container.RegisterType<MainView, MainView>();
-            container.RegisterType<MainController, MainController>();
-
-
-            container.RegisterType<DriverPipeServerController, DriverPipeServerController>(new ContainerControlledLifetimeManager());
-            container.RegisterType<DriverPipeServerController, DriverPipeServerController>(new ContainerControlledLifetimeManager());
-            container.RegisterType<DataController, DataController>(new ContainerControlledLifetimeManager());
-            container.RegisterType<ProcessingPipeline, ProcessingPipeline>(new ContainerControlledLifetimeManager());
-            container.RegisterType<ModuleWrapper, ModuleWrapper>(new ContainerControlledLifetimeManager());
-            container.RegisterInstance<IKalmanFilterModel>(new KalmanFilterModel());
-
-            ApplicationConfiguration.Initialize();
-            var controller = container.Resolve<MainController>();
-            Application.Run(controller.RegisterEventAggregator(container).Initialize().View);
+           
         }
     }
 }
