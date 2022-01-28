a07775e4beca1b44ceb98abc91acf50c2a989190
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-01-28 18:22:34 +0100

    fix of nose_norm

--- src/Modules/PoseClassifier/MediaPipe/utils_mp/positions.py
+++ src/Modules/PoseClassifier/MediaPipe/utils_mp/positions.py
@@ -1,4 +1,4 @@
-from typing import Optional,List,Dict
+from typing import Optional, List, Dict
 import json
 import mediapipe as mp
 from utils.positions_dataclass import Positions
@@ -7,28 +7,32 @@ import numpy as np
 
 
 class PositionHandler:
-    """ use keys from Positions-Class to extract positions from classification
+    """use keys from Positions-Class to extract positions from classification
 
     Returns:
         [Postisions]: [Dataclass with elemets as landmarks ans values of type List[float]]
     """
-    defaultPosition: List[float] = [.0, .0, .0]
-    position_visible_threshold: float = 0.5
-    positions:Dict[str,List[float]] = {field.name:[] for field in fields(Positions())}
-    previous_positions: Positions = Positions()
-    current_positions: Positions = Positions()
 
-
-    def __init__(self, ignore_hidden_points: Optional[bool] = False, 
-                 output_to_file: bool = False,
-                 outputfile: str = "landmarks.txt", 
-                 increase_depth:float = 1
-                 ):
+    def __init__(
+        self,
+        ignore_hidden_points: Optional[bool] = False,
+        output_to_file: bool = False,
+        outputfile: str = "landmarks.txt",
+        increase_depth: float = 1,
+    ):
         # settings
+        self.defaultPosition: List[float] = [0.00123, 0.00123, 0.00123]
+        self.position_visible_threshold: float = 0.3
+        self.positions: Dict[str, List[float]] = {
+            field.name: [] for field in fields(Positions())
+        }
+        self.previous_positions: Positions = Positions()
+        self.current_positions: Positions = Positions()
+
         self.use_visibility_threshold = ignore_hidden_points
         if self.use_visibility_threshold:
             print("set small values to 0")
-            
+
         self.file_output = output_to_file
         self.export_file = outputfile
 
@@ -42,10 +46,10 @@ class PositionHandler:
         """loads keypoint names from dataclass Positions and fills them with values"""
         self.last_positions = self.current_positions
         positions = self._load_positions(results=results)
-        
 
         self._calc_landmarks(positions)
         if self.positions:
+
             self._norm_position_to_nose()
             self.current_positions = Positions(**self.positions)
             if self.file_output:
@@ -53,38 +57,47 @@ class PositionHandler:
             return self.current_positions
 
     def _load_positions(self, results):
-        """ extracts the landmarks from the result"""
-        positions: dict[str,List[float]] = {}
+        """extracts the landmarks from the result"""
+        positions: dict[str, List[float]] = {}
         for field in fields(Positions):
-            positions[field.name] = results.pose_world_landmarks.landmark[self.mp_pose.PoseLandmark[field.name]]
+            positions[field.name] = results.pose_world_landmarks.landmark[
+                self.mp_pose.PoseLandmark[field.name]
+            ]
         return positions
 
     def _set_name_warning(self, name: str):
         """keep track of landmarks that cause trouble"""
         self.not_visible_names.append(name)
         print(f"{name=} caused an AttributeError \n")
-        
+
     def _get_pose_center(self):
         """Calculates pose center as point between hips."""
-        
-        left_hip = np.array(self.positions["LEFT_HIP"]) # left hip
-        right_hip = np.array(self.positions["RIGHT_HIP"]) # right hip
+
+        left_hip = np.array(self.positions["LEFT_HIP"])  # left hip
+        right_hip = np.array(self.positions["RIGHT_HIP"])  # right hip
         center = (left_hip + right_hip) * 0.5
         return center
-    
+
     def _norm_position_to_nose(self):
         """Normalizes landmarks translation and scale."""
-        #calculate distane between nose and center
-        #pose_center = self._get_pose_center()
+        # calculate distane between nose and center
+        # pose_center = self._get_pose_center()
+
         distance = self.positions["NOSE"]
-        
-        for key,values in self.positions.items():
-            self.positions[key][0] = values[0] - distance[0]
-            self.positions[key][1] = (values[1] - distance[1]) * (-1) # inverse y-axis
-            self.positions[key][2] *= (values[2] - distance[2]) #* self.increase_depth
-            self.positions[key][3] = values[3]
-        
-        
+        assert distance[1] != 0.0
+        # print("Lear after", self.positions["LEFT_EAR"][1])
+        # print("KNEE", self.positions["LEFT_KNEE"][1])
+        # print(self.positions.items())
+        for key, values in self.positions.items():
+
+            # print(key, distance)
+            self.positions[key] = [
+                values[0] - distance[0],
+                distance[1] - values[1],
+                values[2] - distance[2],
+                values[3],
+            ]
+
     def _calc_landmarks(self, landmarks):
         """Listify positions
 
@@ -95,18 +108,17 @@ class PositionHandler:
             return
         for key, value in landmarks.items():
             if value:
-                if self.use_visibility_threshold and value.visibility < self.position_visible_threshold:
+                if (
+                    self.use_visibility_threshold
+                    and value.visibility < self.position_visible_threshold
+                ):
                     self.positions[key] = self.defaultPosition
                 else:
                     self.positions[key] = [value.x, value.y, value.z, value.visibility]
-                
-                    
+
             else:
                 continue
-            
 
     def write_file(self):
         with open(self.export_file, "w+") as f:
             json.dump(self.positions, f, indent=4)
-
- 
